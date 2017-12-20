1530ec52204e9289f52ad1f80cdf1b305fa60183
boggerbee <anders@kreutzer-jorgensen.com>
2017-12-20 17:31:07 +0100

    updated graph with date and time parameters

--- app/js/graph.js
+++ app/js/graph.js
@@ -1,8 +1,17 @@
 var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
 var graphWidth = 500;
-var hrsBack = 24;
+var now = new Date();
+var endDate = now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
+now.setDate(now.getDate()-2);
+var startDate = now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
+var startTime = '00:00'; 
+var endTime = '23:59'; //now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
 
-document.getElementById("hrs").value = hrsBack;
+
+document.getElementById("start_date").value = startDate;
+document.getElementById("end_date").value = endDate;
+document.getElementById("start_time").value = startTime;
+document.getElementById("end_time").value = endTime;
 
 if(window.innerWidth <= 800) {
 	graphWidth = 300;
@@ -10,25 +19,19 @@ if(window.innerWidth <= 800) {
 var globals = {};
 
 function updateGraphs() {
-	hrsBack = document.getElementById("hrs").value;
+	startDate = document.getElementById("start_date").value+'T'+document.getElementById("start_time").value+':00+01:00';
+	endDate = document.getElementById("end_date").value+'T'+document.getElementById("end_time").value+':00+01:00';
 	
-	d3.json('http://data.kreutzer.no/dataserver/api/graphmarkers?id=Almedalen25&hrs='+hrsBack, function(data) {
-		data.forEach(function (d) {
-		var str = d.date; 
-		if (str != null) {
-			d.date = parseTime(str.substring(0, str.length - 9));
-		}
-		});	
+	var params = 'id=Almedalen25&s='+encodeURIComponent(startDate)+'&e='+encodeURIComponent(endDate);
+	console.log(params);
+	
+	d3.json('http://data.kreutzer.no/dataserver/api/markers?'+params, function(data) {
+		MG.convert.date(data, "date","%d-%m-%Y %H:%M:%S");
 		globals.data = data;
+		console.log("marker data len: "+data.length);
 	});
-	d3.json('http://data.kreutzer.no/dataserver/api/graphlevel?id=Almedalen25&hrs='+hrsBack, function(data) {
-		data.forEach(function (d) {
-		var str = d.date; 
-		if (str != null) {
-			d.date = parseTime(str.substring(0, str.length - 9));
-		}
-		});	
-
+	d3.json('http://data.kreutzer.no/dataserver/api/level?'+params, function(data) {
+		MG.convert.date(data, "date","%d-%m-%Y %H:%M:%S");
 		MG.data_graphic({
 			title: "Level plot",
 			//description: "Plot of tank level last x days",
@@ -44,14 +47,8 @@ function updateGraphs() {
 			target: '#levelgraph'
 		});
 	});
-	d3.json('http://data.kreutzer.no/dataserver/api/graphflow?id=Almedalen25&hrs='+hrsBack, function(data) {
-		data.forEach(function (d) {
-		var str = d.date; 
-		if (str != null) {
-			d.date = parseTime(str.substring(0, str.length - 9));
-		}
-		});	
-
+	d3.json('http://data.kreutzer.no/dataserver/api/flow?'+params, function(data) {
+		MG.convert.date(data, "date","%d-%m-%Y %H:%M:%S");
 		MG.data_graphic({
 			title: "Flow plot",
 			data: data,
--- app/js/init.js
+++ app/js/init.js
@@ -3,5 +3,29 @@
 
     $('.button-collapse').sideNav();
 	updateGraphs();
+	
+	$('.datepicker').pickadate({
+		format: 'yyyy-mm-dd',
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 2, // Creates a dropdown of 15 years to control year,
+		today: 'Today',
+		clear: 'Clear',
+		close: 'Ok',
+		closeOnSelect: true // Close upon selecting a date,
+	});
+	
+	$('.timepicker').pickatime({
+//		timeFormat: 'HH:mm:ss',
+		default: 'now', 	// Set default time: 'now', '1:30AM', '16:30'
+		fromnow: 0,       	// set default time to * milliseconds from now (using with default = 'now')
+		twelvehour: false, 	// Use AM/PM or 24-hour format
+		donetext: 'OK', 	// text for done-button
+		cleartext: 'Clear', // text for clear-button
+		canceltext: 'Cancel', // Text for cancel-button
+		autoclose: false, 	// automatic close timepicker
+		ampmclickable: true, // make AM PM clickable
+		aftershow: function(){} //Function for after opening timepicker
+	  });
+  
   }); // end of document ready
 })(jQuery); // end of jQuery name space
